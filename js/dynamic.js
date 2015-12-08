76811598952419d5ee2d9ecbe6adae10362ebb11
Alexandr <justdealman@gmail.com>
2015-12-08 11:14:28 +0700

    original

---
+++ js/dynamic.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function() {
+	if ( $('.introduction').length > 0 ) {
+		$('.introduction').css({
+			'background': 'url("'+$('.introduction').find('img').attr('src')+'") no-repeat center center',
+			'-webkit-background-size': 'cover',
+			'-moz-background-size': 'cover',
+			'-o-background-size': 'cover',
+			'background-size': 'cover'
+		});
+	}
+	if ( $('.news-b').length > 0 ) {
+		$('.news-b div .picture').each(function() {
+			$(this).parent().addClass('imaged');
+		});
+	}
+	$('.subs-open').bind('click', function(event) {
+		event.preventDefault();
+		$('.subscribe').stop().fadeIn(200);
+	});
+	$('.subscribe .close').bind('click', function(event) {
+		event.preventDefault();
+		$(this).parent().stop().fadeOut(200);
+	});
+	$('.reviews-i .prev').bind('click', function(event) {
+		event.preventDefault();
+		$('.reviews-i .next').removeClass('disabled');
+		var t = $(this).parents('.reviews-i').children('div');
+		if ( t.find('.active').index()+1 > 1 ) {
+			t.find('.active').removeClass().prev('ul').addClass('active');
+		}
+		if ( t.find('.active').index()+1 < 2 ) {
+			$('.reviews-i .prev').addClass('disabled');
+		}
+	});
+	$('.reviews-i .next').bind('click', function(event) {
+		event.preventDefault();
+		$('.reviews-i .prev').removeClass('disabled');
+		var t = $(this).parents('.reviews-i').children('div');
+		if ( t.find('.active').index()+1 < t.find('ul').size() ) {
+			t.find('.active').removeClass().next('ul').addClass('active');
+		}
+		if ( t.find('.active').index()+1 >= t.find('ul').size() ) {
+			$('.reviews-i .next').addClass('disabled');
+		}
+	});
+	$('input, textarea').each(function() {
+		$(this).data('holder', $(this).attr('placeholder'));
+		$(this).focusin(function() {
+			$(this).attr('placeholder', '');
+		});
+		$(this).focusout(function() {
+			$(this).attr('placeholder', $(this).data('holder'));
+		});
+	});
+});
